c5f99366414521fbb8e22ca0427e5b01e4f7fb0e
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-03-13 12:34:14 +0600

    feat: add

--- src/pages/main/components/header/Header.tsx
+++ src/pages/main/components/header/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import s from "./Header.module.scss"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className={s.Header}>
+      <div className="container">
+        <div className={s.content}>
+          <h1>day 1</h1>
+        </div>
+      </div>
+    </header>
   )
 }
 
