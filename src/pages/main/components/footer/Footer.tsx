7a4d96141fc4e24ce7b9a55d22bd904830824ea6
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-03-13 12:37:33 +0600

    feat: add

--- src/pages/main/components/footer/Footer.tsx
+++ src/pages/main/components/footer/Footer.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import s from "./Footer.module.scss"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={s.Footer}>
+      <div className="container">
+        <div className={s.content}>
+          <h1>day1 footer</h1>
+        </div>
+      </div>
+    </footer>
   )
 }
 
