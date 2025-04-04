61371fbe1704070267ead993afeb0c4c388c5f6f
Abdurakhman <abdibaitovabdurahman@gmail.com>
2025-04-04 10:48:44 +0600

    feat: add

--- src/pages/main/components/header/Header.tsx
+++ src/pages/main/components/header/Header.tsx
@@ -436,6 +436,9 @@ const Header = () => {
           <h1>day 22</h1> 
           <h1>day 22</h1> 
           <h1>day 22</h1> 
+          <h1>day 22</h1> 
+          <h1>day 22</h1> 
+          <h1>day 22</h1> 
         </div>
       </div>
     </header>
